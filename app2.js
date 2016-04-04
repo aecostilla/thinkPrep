94ba6a7196786ef1c267549057a90933b8ea69be
Abel Costilla <aecostilla@gmail.com>
2016-04-03 22:55:03 -0500

    added World Translator Hello World function

--- app2.js
+++ app2.js
@@ -1,4 +1,6 @@
-var fizzBuzz = function(start,stop) {
+        // Fizz Buzz Challenge
+
+/* var fizzBuzz = function(start,stop) {
 for(var i = start; i <= stop; i++) {
    
     if (i % 3 === 0 && i % 5 === 0) {
@@ -13,5 +15,57 @@ for(var i = start; i <= stop; i++) {
     else {
         console.log(i);
         };
+    };
+}; */
+
+    // The Grade Assigner 
+
+/* var myGrade = function(i) {
+    
+    if (i >= 90) {
+        console.log("A");
+    }
+    
+    else if (i >= 80) {
+        console.log("B");
+    }
+    
+    else if (i >= 70) {
+        console.log("C");
+    }
+    
+    else if (i >= 60) {
+        console.log("D");
+    }
+    
+    else {
+        console.log("F");
+    }
 };
-};
+
+*/
+
+        // The World Translator
+
+/* var es = "spanish";
+var de = "german";
+var fr = "french";
+
+var wTranslate = function(lan){
+    
+    if (lan === "spanish"){
+        console.log("Hola Mundo");
+    }
+    else if (lan === "german"){
+        console.log("Hallo Welt");
+    }
+    else if (lan === "french"){
+        console.log("Bonjour le Monde");
+    }
+    else {
+        console.log("Hello World")
+    };
+}; */
+
+        // The Pluralizer
+
