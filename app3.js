1b59e6d4e6d47b5586483cfa9e805dd621c1bcd6
Abel Costilla <aecostilla@gmail.com>
2016-04-05 17:31:19 -0500

    Added app3

---
+++ app3.js
@@ -0,0 +1,26 @@
+ // Top Ten Countdown 
+/*
+var whileTest = function(i){
+    while(i > 0) {
+        i--;
+        console.log(i);
+    }
+};
+*/
+
+        // While loop (did not get...)
+
+/*
+
+var cars = ["BMW","Volvo","Saab","Ford"];
+var i = 0;
+var text ="";
+
+while(cars[i]){
+    text += cars[i] + " ";
+    i++;
+}
+
+console.log(text);
+
+*/
