335da6513242f4430abec394855738a1d3c96ec7
Abel Costilla <aecostilla@gmail.com>
2016-04-12 17:45:00 -0500

    Update on index and app3

--- app3.js
+++ app3.js
@@ -24,3 +24,64 @@ while(cars[i]){
 console.log(text);
 
 */
+
+        // The even/odd Reporter
+
+/*
+
+var reporter = function(start,stop,phrase){
+    for (var i = start; i <= stop; i++) {
+        if(i % 2 === 0) {
+            console.log(i + " " + phrase);
+        }
+        
+        else {
+            console.log(i + " is odd");
+        }
+    };
+};
+
+reporter(0,20,"is Even");
+
+*/
+
+
+/*
+        // Console log the Bigger number
+
+var bigger = function(x,y) {
+    if (x > y){
+        console.log(x + " is greater");
+    }
+    
+    else if (y > x) {
+        console.log(y + " is greater");
+    }
+    
+    else if (x === y){
+        console.log("non is greater");
+    }
+};
+
+*/
+
+
+/*
+for(var n = 0; n <= 10; n++){
+    console.log(n *9);
+};
+*/
+
+var output = "";
+for(var b = 0; b <=10; b++){
+        output += "\n";
+    for(var a = 1; a <=10; a++){
+        output += a*b;
+    };
+}
+
+console.log(output);
+
+
+
+
